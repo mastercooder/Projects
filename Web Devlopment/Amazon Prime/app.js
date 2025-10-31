c9b3fe8f17565258f1937ff0f09615f5d30b0d0d
mastercooder <tejesh.patel256@gmail.com>
2025-10-31 13:54:30 +0530

    31/10/2025

---
+++ Web Devlopment/Amazon Prime/app.js
@@ -0,0 +1,57 @@
+
+// --------- Navigation ------------
+
+// Subscription
+const navSub = document.querySelector('.nav-sub');
+const iconImg = document.getElementById('icon-img');
+const text = document.querySelector('.nav-sub-txt h3');
+navSub.addEventListener('mouseenter', () => {
+  navSub.style.backgroundColor = 'white';
+  text.style.color = 'black';
+  iconImg.style.backgroundImage = 'url("Icons/Subscriptions2.png")';
+});
+navSub.addEventListener('mouseleave', () => {
+  navSub.style.backgroundColor = 'transparent';
+  text.style.color = 'white';
+  iconImg.style.backgroundImage = 'url("Icons/Subscriptions.png")';
+});
+
+// Search
+const navsearch = document.querySelector('.nav-search');
+const navsericon = document.getElementById('nav-ser-logo');
+navsearch.addEventListener('mouseenter', () =>{
+    navsearch.style.backgroundColor = 'white';
+    navsericon.style.backgroundImage = 'url(/Icons/search2.png)'
+});
+navsearch.addEventListener('mouseleave', () =>{
+    navsearch.style.backgroundColor = 'transparent';
+    navsericon.style.backgroundImage = 'url(/Icons/search.png)'
+});
+
+// Language
+const navlan = document.querySelector('.nav-lan');
+const navlanicon = document.getElementById('lan-img');
+navlan.addEventListener('mouseenter', () =>{
+    navlan.style.backgroundColor = 'white';
+    navlan.style.color = 'black';
+    navlanicon.style.backgroundImage = 'url(/Icons/dropdown2.png)'
+});
+navlan.addEventListener('mouseleave', () =>{
+    navlan.style.backgroundColor = 'transparent';
+    navlan.style.color = 'white';
+    navlanicon.style.backgroundImage = 'url(/Icons/dropdown.png)'
+});
+
+// Multipleselect
+const mulsel = document.querySelector('.nav-mul-sel');
+const mulselicon = document.getElementById('mulsel-img');
+mulsel.addEventListener('mouseenter', () =>{
+    mulsel.style.backgroundColor = 'white';
+    mulsel.style.color = 'black';
+    mulselicon.style.backgroundImage = 'url(/Icons/mutliselector2.png)'
+});
+mulselicon.addEventListener('mouseleave', () =>{
+    mulsel.style.backgroundColor = 'transparent';
+    mulsel.style.color = 'white';
+    mulselicon.style.backgroundImage = 'url(/Icons/mutliselector.png)'
+});
